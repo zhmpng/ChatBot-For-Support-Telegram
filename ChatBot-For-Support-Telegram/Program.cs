317a3a10e4d43a7edd89b3761b476b6cc31c7217
Mikhail Zhmylev <zhmylev_m@outlook.com>
2022-03-10 21:36:05 +0300

    init bot commit
    First commit with proj files

---
+++ ChatBot-For-Support-Telegram/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.ComponentModel;
+
+namespace ChatBotForSupport
+{
+    internal static class Program
+    {
+        public static BackgroundWorker bw;
+        private static readonly string publicKey = Configuration.Default.publicKey;
+        private static int offset = 0;
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new Form1());
+
+            var inner = Task.Factory.StartNew(() =>  // вложенная задача
+            {
+                Bot();
+            });
+            Console.WriteLine("Бот был успешно запущен!");
+        }
+
+        private static async void Bot()
+        {
+            bw = new BackgroundWorker();
+            bw.DoWork += bw_DoWork;
+
+            var boolText = string.IsNullOrEmpty(publicKey);
+
+            if (!boolText && bw.IsBusy != true)
+            {
+                bw.RunWorkerAsync(publicKey);
+            }
+        }
+
+        static async void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+        }
+    }
+}
