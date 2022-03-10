abcc428993a7fece9ecb568015b74cc47b514d62
Mikhail Zhmylev <zhmylev_m@outlook.com>
2022-03-10 23:52:00 +0300

    add: message function

--- ChatBot-For-Support-Telegram/Program.cs
+++ ChatBot-For-Support-Telegram/Program.cs
@@ -1,45 +1,135 @@
-﻿using System.ComponentModel;
+﻿using ChatBotForSupport.UpdateHandlers;
+using System.ComponentModel;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace ChatBotForSupport
 {
     internal static class Program
     {
-        public static BackgroundWorker bw;
-        private static readonly string publicKey = Configuration.Default.publicKey;
-        private static int offset = 0;
+        public static BackgroundWorker _bw;
+        private static readonly string _publicKey = Configuration.Default.publicKey;
+        private static int _offset = 0;
+        private static bool _stopProgram = false;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
-            // To customize application configuration such as set high DPI settings or default font,
-            // see https://aka.ms/applicationconfiguration.
-            ApplicationConfiguration.Initialize();
-            Application.Run(new Form1());
-
-            var inner = Task.Factory.StartNew(() =>  // вложенная задача
+            //ApplicationConfiguration.Initialize();
+            //Application.Run(new Form1());
+            
+            var inner = Task.Factory.StartNew(() =>
             {
                 Bot();
             });
             Console.WriteLine("Бот был успешно запущен!");
+            inner.Wait();
+            do { } while (!_stopProgram);
         }
 
         private static async void Bot()
         {
-            bw = new BackgroundWorker();
-            bw.DoWork += bw_DoWork;
+            _bw = new BackgroundWorker();
+            _bw.DoWork += _bw_DoWork;
 
-            var boolText = string.IsNullOrEmpty(publicKey);
+            var boolText = string.IsNullOrEmpty(_publicKey);
 
-            if (!boolText && bw.IsBusy != true)
+            if (!boolText && _bw.IsBusy != true)
             {
-                bw.RunWorkerAsync(publicKey);
+                _bw.RunWorkerAsync(_publicKey);
             }
         }
 
-        static async void bw_DoWork(object sender, DoWorkEventArgs e)
+        static async void _bw_DoWork(object sender, DoWorkEventArgs e)
         {
+            var worker = sender as BackgroundWorker;
+            var key = e.Argument as String;
+            var Bot = new TelegramBotClient(key);
+            await Bot.SetWebhookAsync("");
+            var preUpdates = await Bot.GetUpdatesAsync(-1);
+            _offset = preUpdates.Select(s => s.Id).LastOrDefault();
+            _offset += 1;
+
+            try {
+                bool repeat = true;
+                while (repeat)
+                {
+                    var updates = new Update[0];
+                    try
+                    {
+                        updates = await Bot.GetUpdatesAsync(_offset);
+                    }
+                    catch (TaskCanceledException)
+                    {
+                        // Don't care
+                    }
+                    catch (Exception ex)
+                    {
+                        await Bot.SendTextMessageAsync("441224506", "ERROR WHILE GETTIGN UPDATES - " + ex);
+                    }
+                    foreach (var update in updates)
+                    {
+                        _offset = update.Id + 1;
+                        switch (update.Type)
+                        {
+                            case UpdateType.Message:
+                                if (update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text && update.Message.Text.Replace("@WeekendFacePolice_bot", "").ToLower() == ("/restart"))
+                                {
+                                    repeat = false;
+                                    _offset = 0;
+                                    throw new Exception($"Бот был перезапущен при помощи команды /restart , данную команду запустил @{update.Message.From.Username} - {update.Message.From.FirstName}");
+                                }
+                                if (update.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text && update.Message.Text.Replace("@WeekendFacePolice_bot", "").ToLower() == ("/stop"))
+                                {
+                                    repeat = false;
+                                    _offset = 0;
+                                    _stopProgram = true;
+                                    throw new Exception($"Бот был перезапущен при помощи команды /restart , данную команду запустил @{update.Message.From.Username} - {update.Message.From.FirstName}");
+                                }
+                                MessageBase.MessageHandler(update, Bot);
+                                break;
+                            case UpdateType.CallbackQuery:
+                                break;
+                            case UpdateType.InlineQuery:
+                                break;
+                            case UpdateType.EditedMessage:
+                                break;
+                            case UpdateType.EditedChannelPost:
+                                break;
+                            case UpdateType.ChosenInlineResult:
+                                break;
+                            case UpdateType.ChannelPost:
+                                break;
+                            case UpdateType.Poll:
+                                break;
+                            case UpdateType.PollAnswer:
+                                break;
+                            case UpdateType.PreCheckoutQuery:
+                                break;
+                            case UpdateType.ShippingQuery:
+                                break;
+                            case UpdateType.Unknown:
+                                break;
+                            case UpdateType.MyChatMember:
+                                break;
+                            case UpdateType.ChatJoinRequest:
+                                break;
+                            case UpdateType.ChatMember:
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                await Bot.SendTextMessageAsync("441224506", ex.Message + "-" + ex.StackTrace);
+                _bw.RunWorkerAsync(_publicKey);
+            }
         }
     }
 }
---
+++ ChatBot-For-Support-Telegram/UpdateHandlers/MessageHandler.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.ReplyMarkups;
+
+namespace ChatBotForSupport.UpdateHandlers
+{
+    public static class MessageBase
+    {
+        private static readonly char[] deaf = new char[] { '_', '*', '[', ']', '(', ')', '~', '`', '>', '#', '+', '-', '=', '|', '{', '}', '.', '!', '<' };
+        public async static void MessageHandler(Update update, TelegramBotClient bot)
+        {
+            var message = update.Message ?? null;
+            if (message == null) return;
+            else
+                switch (message?.Type)
+                {
+                    case MessageType.Text:
+                        //await bot.SendTextMessageAsync("441224506", message?.Text);
+                        string replacedMessageName = update.Message.From.FirstName + " " + update.Message.From.LastName;
+                        foreach (char c in deaf)
+                        {
+                            replacedMessageName = replacedMessageName.Replace(c.ToString(), "");
+                        }
+                        var keyboard = new Telegram.Bot.Types.ReplyMarkups.InlineKeyboardMarkup(new[]
+                        {
+                            new []
+                            {
+                                InlineKeyboardButton.WithCallbackData("Ответить")
+                            }
+                        });
+                        await bot.SendTextMessageAsync(message.Chat.Id, $"Отправитель: [{replacedMessageName}](tg://user?id={message?.From?.Id}) \nТекст сообщения: {message?.Text}", replyMarkup: keyboard, parseMode: ParseMode.Markdown);
+                        break;
+
+                }
+                //if (message?.Type == MessageType.Text && message?.Text?.Replace("@BeOpenChatBot", "").ToLower() == ("/restart"))
+                //{
+                //    repeat = false;
+                //    _offset = 0;
+                //    throw new Exception($"Бот был перезапущен при помощи команды /restart , данную команду запустил @{message?.From?.Username} - {message?.From?.FirstName}");
+                //}
+        }
+    }
+}
